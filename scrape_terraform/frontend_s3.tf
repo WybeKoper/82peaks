879410db8312ec0c27669384e6d14bcd4c7abb39
wybekoper <wybe.koper@gmail.com>
2023-10-15 17:50:44 +0200

    Added static website hosting with s3

---
+++ scrape_terraform/frontend_s3.tf
@@ -0,0 +1,62 @@
+
+resource "aws_s3_bucket" "frontend_bucket" {
+  bucket        = "peaks-bucket-frontend-${random_pet.bucket_name.id}"
+  force_destroy = true
+}
+
+resource "aws_s3_bucket_website_configuration" "example" {
+  bucket = aws_s3_bucket.frontend_bucket.id
+
+  index_document {
+    suffix = "index.html"
+  }
+
+}
+resource "aws_s3_bucket_ownership_controls" "example" {
+  bucket = aws_s3_bucket.frontend_bucket.id
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "example" {
+  bucket = aws_s3_bucket.frontend_bucket.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_acl" "example" {
+  depends_on = [
+    aws_s3_bucket_ownership_controls.example,
+    aws_s3_bucket_public_access_block.example,
+  ]
+
+  bucket = aws_s3_bucket.frontend_bucket.id
+  acl    = "public-read"
+}
+
+resource "aws_s3_bucket_policy" "allow_pulic_access" {
+  bucket = aws_s3_bucket.frontend_bucket.id
+  policy = data.aws_iam_policy_document.website_policy.json
+}
+
+
+data "aws_iam_policy_document" "website_policy" {
+  statement {
+    actions = [
+      "s3:GetObject",
+      "s3:ListBucket"
+    ]
+    principals {
+      identifiers = ["*"]
+      type        = "AWS"
+    }
+    resources = [
+        aws_s3_bucket.frontend_bucket.arn,
+      "${aws_s3_bucket.frontend_bucket.arn}/*",
+    ]
+  }
+}
